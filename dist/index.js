98c3eaeb6c6fe21dfe73cdab77c4beae771a374c
omkar suryawanshi <omkar876742@gmail.com>
2025-04-03 00:33:35 +0530

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("hello world");
+var age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+console.log("hello world");
+var age: number = 20;
+if(age< 50)
+    age += 10;
+ console.log(age);
